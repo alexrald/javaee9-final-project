9162f1310a6738d07086d3999425e743c8986411
alexrald <alexral.ee@gmail.com>
2022-03-26 11:22:44 +0200

    [fix] - CORS (Cross-Origin Resource Sharing) works

--- social-network-backend/src/main/java/com/javaee9/javaee9finalproject/controller/ConfigController.java
+++ social-network-backend/src/main/java/com/javaee9/javaee9finalproject/controller/ConfigController.java
@@ -11,7 +11,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @Slf4j
 @RequestMapping("/config")
-@CrossOrigin("*")
+@CrossOrigin("http://localhost:4200")
+// CORS - Cross-Origin Resource Sharing
+// @CrossOrigin annotation allows browser to process requests from a different origin
+// In this case, the resource is shared between localhost:8080 (backend) and localhost:4200 (frontend)
+// See more in https://developer.mozilla.org/en-US/docs/Web/Security/Same-origin_policy
+// and https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS/Errors
+
 public class ConfigController {
 
     private final ConfigService configService;
